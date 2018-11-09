3f4ad0b07b7dffc2cbde08099d0a38d221066020
20zinnm <meyerzinn@gmail.com>
2018-11-09 17:34:58 -0600

    fix constructor

--- evaluator.py
+++ evaluator.py
@@ -5,7 +5,7 @@ class Evaluator(evaluator_pb2_grpc.EvaluatorServicer):
     def Evaluate(self, request, context):
         # build Keras model
         for layer in request.layers:
-            layer = protocol_pb2.Layer()
+            layer = protocol_pb2.Layer(layer)
             typ = request.WhichOneof("definition")
             if (typ == None):
                 continue
@@ -17,8 +17,8 @@ class Evaluator(evaluator_pb2_grpc.EvaluatorServicer):
             elif (typ == "dropout"):
                 # do something here
             elif (typ == "flatten"):
-
                 
+
         # train the model
 
         # send progress updates
