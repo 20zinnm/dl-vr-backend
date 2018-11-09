33e7606d3ad76be239294ff6e192b38060315e40
20zinnm <meyerzinn@gmail.com>
2018-11-09 16:43:09 -0600

    add command to readme

--- evaluator.py
+++ evaluator.py
@@ -1,12 +1,12 @@
-import protocol_pb2_grpc;
-import protocol_pb2;
+import evaluator_pb2;
+import evaluator_pb2_grpc;
 
 class EvaluatorService(protocol_pb2_grpc.EvaluatorService):
     def Evaluate(self, request, context):
         # build Keras model
         for layer in request.layers:
             layer = protocol_pb2.Layer()
-        
+
         # train the model
 
         # send progress updates
