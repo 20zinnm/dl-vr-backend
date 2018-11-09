c71d4b0623ec1574d7ed01c78f9bc06af01e5562
20zinnm <meyerzinn@gmail.com>
2018-11-09 16:42:03 -0600

    add more layers to protocol

--- evaluator.py
+++ evaluator.py
@@ -1,6 +1,14 @@
-import protocol_pb2_grpc as evaluator_service;
-import protocol_pb2 as evaluator_messages;
+import protocol_pb2_grpc;
+import protocol_pb2;
 
-class EvaluatorService(evaluator_service.EvaluatorService):
+class EvaluatorService(protocol_pb2_grpc.EvaluatorService):
     def Evaluate(self, request, context):
+        # build Keras model
+        for layer in request.layers:
+            layer = protocol_pb2.Layer()
+        
+        # train the model
+
+        # send progress updates
+        yield protocol_pb2.ProgressUpdate(accuracy = 1);
         
