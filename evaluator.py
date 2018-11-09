e618502c638910a506b8069165a928f5c82550d9
20zinnm <meyerzinn@gmail.com>
2018-11-09 17:07:43 -0600

    working server

--- evaluator.py
+++ evaluator.py
@@ -1,9 +1,5 @@
 import evaluator_pb2
 import evaluator_pb2_grpc
-import grpc
-from concurrent import futures
-import time
-_ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 class Evaluator(evaluator_pb2_grpc.EvaluatorServicer):
     def Evaluate(self, request, context):
@@ -22,18 +18,3 @@ class Evaluator(evaluator_pb2_grpc.EvaluatorServicer):
 
         # send progress updates
         # yield evaluator_pb2_grpc.ProgressUpdate(accuracy = 1)
-        
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    evaluator_pb2_grpc.add_EvaluatorServicer_to_server(Evaluator(), server)
-    server.add_insecure_port('[::]:50051')
-    server.start()
-    try:
-        while True:
-            time.sleep(_ONE_DAY_IN_SECONDS)
-    except KeyboardInterrupt:
-        server.stop(0)
-
-
-if __name__ == '__main__':
-    serve()
